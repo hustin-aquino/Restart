5d6bcb1b44f64521cd984f63f9ee4cea9bb1aa7c
hustin-aquino <justinmosesbitancor@gmail.com>
2024-10-06 16:34:50 +0800

    Initial Commit

---
+++ Restart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by justin.bitancor on 10/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by justin.bitancor on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
